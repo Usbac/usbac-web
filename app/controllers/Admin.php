8f402bed11549c5fb6cd55153ef28c625f6dfb0e
Alejandro <castillo.alex97@outlook.com>
2021-09-14 10:43:13 -0400

    Remove old code in admin controller

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -11,24 +11,6 @@ class Admin extends \Wolff\Core\Controller
 
 
     /**
-     * Options.
-     */
-    public function options()
-    {
-        $msg = '';
-        if (Session::has('msg')) {
-            $msg = Session::get('msg');
-            Session::unset('msg');
-        }
-
-        View::render('admin/options', [
-            'lang' => Language::get('admin'),
-            'msg'  => $msg,
-        ]);
-    }
-
-
-    /**
      * Login.
      */
     public function login($req, $res)
